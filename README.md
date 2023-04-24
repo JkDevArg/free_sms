8d29b49234381485a3de533a99aa2daa15f1e90c
BuildTools <unconfigured@null.spigotmc.org>
2023-04-23 17:16:59 -1000

    update async/await indexjs

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # free_sms
 
-
 use this
 
 https://github.com/machine1337/fake-sms/blob/main/fakesms.py
+
 https://textbelt.com/
+
--- javascript/index.js
+++ javascript/index.js
@@ -4,35 +4,32 @@ const uri = "https://textbelt.com/text";
 document.getElementById("send_message").addEventListener("click", __init);
 var btn_send = document.getElementById("send_message");
 
-function __init() {
+async function __init() {
     btn_send.innerHTML = "Wait...";
     btn_send.classList.add("disabled");
     const phone = document.getElementById("phone_number").value;
     const message = document.getElementById("message").value;
 
-    const result = sendMessage(phone, message);
-    console.log(result)
-    /* if(result){
+    const result = await sendMessage(phone, message);
+    if(result){
         var res = document.getElementById("response_function");
         res.classList.remove("d-none");
         res.innerHTML = result;
-    } */
+    }
 }
 
-function sendMessage(phone, message) {
-    fetch(uri, {
+async function sendMessage(phone, message) {
+    const response = await fetch(uri, {
         method: "post",
-        headers: { "Content-Type": "application/json" },
+        headers: {
+            "Content-Type": "application/json"
+        },
         body: JSON.stringify({
             phone: phone,
             message: message,
             key: key,
         }),
-    })
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        return data.error;
     });
+    const data = await response.json();
+    return data.error;
 }
